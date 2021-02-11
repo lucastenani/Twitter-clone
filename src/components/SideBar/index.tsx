e4838ac1c564ffbd6bc052739b8be1cead2401d6
Lucas Tenani <lucas.tenani3@gmail.com>
2021-02-11 18:52:39 -0300

    The list component that will compose blocks like Who to follow or What’s happening was created on the right side

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import List from '../List';
 import {
   Container,
   SearchWrapper,
@@ -15,7 +15,22 @@ const SideBar: React.FC = () => {
         <SearchIcon />
       </SearchWrapper>
       <Body>
-        <p>{'Lorem ipsum dolor sit amet'.repeat(90)}</p>
+        <List
+          title="What’s happening"
+          elements={[
+            <h1>Test</h1>,
+            <h1>Test</h1>,
+            <h1>Test</h1>
+          ]}
+        />
+        <List
+          title="Who to follow"
+          elements={[
+            <h1>Test</h1>,
+            <h1>Test</h1>,
+            <h1>Test</h1>
+          ]}
+        />
       </Body>
     </Container>
   );

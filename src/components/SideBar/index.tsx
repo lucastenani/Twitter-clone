4f743d12c04a9a5f848ce697d58993e06033a257
Lucas Tenani <lucas.tenani3@gmail.com>
2021-02-11 23:03:50 -0300

    I inserted the React Sticky Box library to similar the Twitter scroll

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import StickyBox from 'react-sticky-box';
 import List from '../List';
 import FollowSuggestion from '../FollowSuggestion';
 import News from '../News';
@@ -16,24 +17,42 @@ const SideBar: React.FC = () => {
         <SearchInput placeholder="Search Twitter" />
         <SearchIcon />
       </SearchWrapper>
-      <Body>
-        <List
-          title="What’s happening"
-          elements={[
-           <News />,
-           <News />,
-           <News />
-          ]}
-        />
-        <List
-          title="Who to follow"
-          elements={[
-            <FollowSuggestion name="Nick Fury" nickname="@fury_nick" />,
-            <FollowSuggestion name="Thor Odinson" nickname="@odinson" />,
-            <FollowSuggestion name="Steve Rogers" nickname="@roger_steve" />
-          ]}
-        />
-      </Body>
+      <StickyBox>
+        <Body>
+          <List
+            title="What’s happening"
+            elements={[
+            <News />,
+            <News />,
+            <News />
+            ]}
+          />
+          <List
+            title="Who to follow"
+            elements={[
+              <FollowSuggestion name="Nick Fury" nickname="@fury_nick" />,
+              <FollowSuggestion name="Thor Odinson" nickname="@odinson" />,
+              <FollowSuggestion name="Steve Rogers" nickname="@roger_steve" />
+            ]}
+          />
+          <List
+            title="What’s happening"
+            elements={[
+            <News />,
+            <News />,
+            <News />
+            ]}
+          />
+          <List
+            title="What’s happening"
+            elements={[
+            <News />,
+            <News />,
+            <News />
+            ]}
+          />
+        </Body>
+      </StickyBox>
     </Container>
   );
 }
